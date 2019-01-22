19211912bbd2c48768b8c40f7b5fda02cf5025a9
OskarSammalisto <43170804+OskarSammalisto@users.noreply.github.com>
2019-01-22 14:22:23 +0100

    button changes image on throw

--- app/src/main/java/com/example/oskar/yatzy_oskar_sammalisto/DiceRoller.java
+++ app/src/main/java/com/example/oskar/yatzy_oskar_sammalisto/DiceRoller.java
@@ -1,8 +1,25 @@
 package com.example.oskar.yatzy_oskar_sammalisto;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class DiceRoller {
 
-    Random random = new Random();
+   // Random random = new Random();
+
+   public static int[] diceRoller(){
+
+        int[] throwArray = new int[5];
+
+        for(int i = 0; i < throwArray.length; i++){
+           // Random random = new Random();
+            throwArray[i] = (int)(Math.random() * ((6 - 1) +1));
+
+        }
+
+
+
+
+        return throwArray;
+    }
 }
--- app/src/main/java/com/example/oskar/yatzy_oskar_sammalisto/MainActivity.java
+++ app/src/main/java/com/example/oskar/yatzy_oskar_sammalisto/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.oskar.yatzy_oskar_sammalisto;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +16,29 @@ public class MainActivity extends AppCompatActivity {
 
         //DiceEventListener diceSelect = findViewById(R.id.dieOne);
         //diceSelect.setOnPressDie(R.drawable.dice_one_selected);
+
+
     }
+
+    public void rollDice(View view){
+        DiceRoller diceRoller = new DiceRoller();
+        DiceRoller.diceRoller();
+
+        ToggleButton buttonOne = (ToggleButton) findViewById(R.id.toggleButtonDiceOne);
+        buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_one), null,null);
+        //buttonOne.setDrawableTop
+
+
+
+
+        // Log.d("throw", "throw");
+
+
+    }
+
+
+
+
 
 
 }
