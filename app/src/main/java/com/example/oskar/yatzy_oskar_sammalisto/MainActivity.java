bec1e0ae3f31363114fed7ca6f3287107fc6040e
OskarSammalisto <oskarornvs@gmail.com>
2019-01-24 12:33:11 +0100

    random på alla dice

--- app/src/main/java/com/example/oskar/yatzy_oskar_sammalisto/MainActivity.java
+++ app/src/main/java/com/example/oskar/yatzy_oskar_sammalisto/MainActivity.java
@@ -26,30 +26,63 @@ public class MainActivity extends AppCompatActivity {
         int[] throwArray = DiceRoller.diceRoller();
 
 
-        ToggleButton buttonOne = (ToggleButton) findViewById(R.id.toggleButtonDiceOne);
-        if(throwArray[1] == 1)
-        buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_one), null,null);
+        ToggleButton buttonOne =  findViewById(R.id.toggleButtonDiceOne);
+        ToggleButton buttonTwo =  findViewById(R.id.toggleButtonDiceTwo);
+        ToggleButton buttonThree = findViewById(R.id.toggleButtonDiceThree);
+        ToggleButton buttonFour =  findViewById(R.id.toggleButtonDiceFour);
+        ToggleButton buttonFive =  findViewById(R.id.toggleButtonDiceFive);
 
-        else if (throwArray[1] == 2)
-            buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_two), null,null);
+        ToggleButton[] buttonArray = {buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive};
 
-        else if(throwArray[1] == 3)
-            buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_three), null,null);
+        for(int i = 0; i < buttonArray.length; i++){
+            if(!buttonArray[i].isChecked()){
+                if (throwArray[i] == 1)
+                    buttonArray[i].setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_one), null, null);
 
-        else if(throwArray[1] == 4)
-            buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_four), null,null);
+                else if (throwArray[i] == 2)
+                    buttonArray[i].setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_two), null, null);
 
-        else if(throwArray[1] == 5)
-            buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_five), null,null);
+                else if (throwArray[i] == 3)
+                    buttonArray[i].setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_three), null, null);
 
-        else if(throwArray[1] == 6)
-            buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_six), null,null);
+                else if (throwArray[i] == 4)
+                    buttonArray[i].setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_four), null, null);
 
-        else
-            buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_six_selected), null,null);
-        //buttonOne.setDrawableTop
+                else if (throwArray[i] == 5)
+                    buttonArray[i].setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_five), null, null);
 
+                else if (throwArray[i] == 6)
+                    buttonArray[i].setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_six), null, null);
 
+                else
+                    buttonArray[i].setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_six_selected), null, null);
+            }
+        }
+
+//        if(!buttonOne.isChecked()) {
+//            if (throwArray[1] == 1)
+//                buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_one), null, null);
+//
+//            else if (throwArray[1] == 2)
+//                buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_two), null, null);
+//
+//            else if (throwArray[1] == 3)
+//                buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_three), null, null);
+//
+//            else if (throwArray[1] == 4)
+//                buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_four), null, null);
+//
+//            else if (throwArray[1] == 5)
+//                buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_five), null, null);
+//
+//            else if (throwArray[1] == 6)
+//                buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_six), null, null);
+//
+//            else
+//                buttonOne.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.drawable.dice_six_selected), null, null);
+//            //buttonOne.setDrawableTop
+//
+//        }
 
 
         // Log.d("throw", "throw");
